4e7525a6b473e14da22597e972bad73704a3e0be
Aijaz Abbasi <muhammadaijaz76@gmail.com>
2021-06-17 12:00:36 +0500

    disconnect handler

--- dissconnect_handler/main.py
+++ dissconnect_handler/main.py
@@ -7,7 +7,9 @@ dynamodb = boto3.client('dynamodb')
 def handle(event, context):
     connectionId = event['requestContext']['connectionId']
 
+    print("connectionId",connectionId)
+
     # Delete connectionId from the database
-    dynamodb.delete_item(TableName=os.environ['SOCKET_CONNECTIONS_TABLE_NAME'], Key={'connectionId': {'S': connectionId}})
+    dynamodb.delete_item(TableName=os.environ['SOCKET_CONNECTION_TABLE_NAME'], Key={'connectionId': {'S': connectionId}})
 
     return {}
